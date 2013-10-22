3d70329d43b54864da129eb4d56e46e3bd1a4a52
Alex Fix <afix@cs.cornell.edu>
2013-10-22 19:36:15 -0400

    Optimizations in PairwiseCover
    Specializations for D = 4

--- include/pairwise-cover.hpp
+++ include/pairwise-cover.hpp
@@ -41,24 +41,31 @@ class PairwiseCover {
         template <typename QR>
         void ToQuadratic(QR &qr);
 
-    private:
-        typedef uint32_t Assgn;
         struct Clique {
             int size;
             VarId vars[D];
-            std::vector<R> subset_coeffs;
+            R subset_coeffs[1<<D];
             int a_size;
-            std::vector<R> a_coeffs;
-            std::vector<R> b_coeffs;
+            R a_coeffs[1 << ((D+1)/2)];
+            R b_coeffs[1 << (D/2)];
         };
 
-        void ComputeBeta();
-
+    private:
+        typedef uint32_t Assgn;
         R _constantTerm;
         VarId _varCounter;
         std::vector<R> _unaryTerms;
         std::vector<Clique> _cliques;
 };
+
+template <typename Clique>
+inline void ComputeBeta(Clique& c);
+
+template <typename R, int D, typename QR>
+void CliqueToQuadratic(const PairwiseCover<R, D>& pc, const typename PairwiseCover<R, D>::Clique& c, QR& qr);
+
+template <typename R, typename QR>
+void CliqueToQuadratic(const PairwiseCover<R, 4>& pc, const typename PairwiseCover<R, 4>::Clique& c, QR& qr);
 
 template <typename R, int D>
 inline PairwiseCover<R, D>::PairwiseCover()
@@ -120,10 +127,13 @@ inline void PairwiseCover<R, D>::AddClique(const std::vector<VarId>& vars,
     c.size = size;
     for (size_t i = 0; i < size; ++i)
         c.vars[i] = vars[c.size - i - 1];
-    c.subset_coeffs = std::vector<R>(numAssignments, 0);
+    for (size_t i = 0; i < numAssignments; ++i)
+        c.subset_coeffs[i] = 0;
     c.a_size = (size+1)/2;
-    c.a_coeffs = std::vector<R>(1 << c.a_size, 0);
-    c.b_coeffs = std::vector<R>(1 << (size - c.a_size), 0);
+    for (int i = 0; i < (1 << c.a_size); ++i)
+        c.a_coeffs[i] = 0;
+    for (int i = 0; i < (1 << (c.size - c.a_size)); ++i)
+        c.b_coeffs[i] = 0;
 
     // For each boolean assignment, add that to the corresponding monomials
     // with the correct parity
@@ -148,6 +158,7 @@ inline void PairwiseCover<R, D>::AddClique(const std::vector<VarId>& vars,
             }
         }
     }
+    ComputeBeta(c);
 }
 
 template <typename R, int D>
@@ -155,144 +166,185 @@ inline void PairwiseCover<R, D>::AddUnaryTerm(VarId var, R coeff) {
     _unaryTerms[var] += coeff;
 }
 
-template <typename R, int D>
-inline void PairwiseCover<R, D>::ComputeBeta() {
-    for (Clique& c: _cliques) {
-        Assgn everything = (1 << c.size) - 1;
-        Assgn a_vars = (1 << c.a_size) - 1;
-        Assgn b_vars = everything & ~a_vars;
-        for (Assgn term = everything; term > 0; --term) {
-            Assgn term_a = term & a_vars;
-            Assgn term_b = term & b_vars;
-            assert(term_a <= a_vars);
-            assert(term_b <= b_vars);
-            assert((term_a | term_b) == term);
-            if (term_a == 0) {
-                // Then term is subset of B, hence is in pairwise cover
-                // Split it into a singleton and rest of term, singleton
-                // is least set bit
-                assert(term_b == term);
-                // term_a = don't care, it's a singleton, so beta doesn't matter
-                term_b = (term - 1) & term;
-                if (term_b == 0) continue; // Then term was just a singleton anyways
-                c.b_coeffs[term_b >> c.a_size] += 2*abs(c.subset_coeffs[term]) 
-                    + c.b_coeffs[term >> c.a_size];
-            } else if (term_b == 0) {
-                // Then term is subset of A, hence is in pairwise cover
-                // Split it into a singleton and rest of term, singleton
-                // is least set bit
-                assert(term_a == term);
-                term_a = (term - 1) & term;
-                // term_b = don't care, it's a singelton, so beta doesn't matter
-                if (term_a == 0) continue; // Then term was just a singleton anyways
-                c.a_coeffs[term_a] += 2*abs(c.subset_coeffs[term]) 
-                    + c.a_coeffs[term];
-            } else {
-                c.a_coeffs[term_a] += 2*abs(c.subset_coeffs[term]);
-                c.b_coeffs[term_b >> c.a_size] += 2*abs(c.subset_coeffs[term]);
-            }
+template <typename Clique>
+inline void ComputeBeta(Clique& c) {
+    typedef uint32_t Assgn;
+    Assgn everything = (1 << c.size) - 1;
+    Assgn a_vars = (1 << c.a_size) - 1;
+    Assgn b_vars = everything & ~a_vars;
+    for (Assgn term = everything; term > 0; --term) {
+        Assgn term_a = term & a_vars;
+        Assgn term_b = term & b_vars;
+        assert(term_a <= a_vars);
+        assert(term_b <= b_vars);
+        assert((term_a | term_b) == term);
+        if (term_a == 0) {
+            // Then term is subset of B, hence is in pairwise cover
+            // Split it into a singleton and rest of term, singleton
+            // is least set bit
+            assert(term_b == term);
+            // term_a = don't care, it's a singleton, so beta doesn't matter
+            term_b = (term - 1) & term;
+            if (term_b == 0) continue; // Then term was just a singleton anyways
+            c.b_coeffs[term_b >> c.a_size] += 2*abs(c.subset_coeffs[term]) 
+                + c.b_coeffs[term >> c.a_size];
+        } else if (term_b == 0) {
+            // Then term is subset of A, hence is in pairwise cover
+            // Split it into a singleton and rest of term, singleton
+            // is least set bit
+            assert(term_a == term);
+            term_a = (term - 1) & term;
+            // term_b = don't care, it's a singelton, so beta doesn't matter
+            if (term_a == 0) continue; // Then term was just a singleton anyways
+            c.a_coeffs[term_a] += 2*abs(c.subset_coeffs[term]) 
+                + c.a_coeffs[term];
+        } else {
+            c.a_coeffs[term_a] += 2*abs(c.subset_coeffs[term]);
+            c.b_coeffs[term_b >> c.a_size] += 2*abs(c.subset_coeffs[term]);
         }
     }
+}
+
+template <typename R, int D, typename QR>
+void CliqueToQuadratic(const PairwiseCover<R, D>& pc, const typename PairwiseCover<R, D>::Clique& c, QR& qr) {
+    typedef uint32_t Assgn;
+    // TODO(afix): note, there's some waste here, since we
+    // don't actually need nodes for the singeltons / empty
+    // subsets. Shouldn't really affect things too much
+    auto a_nodes = qr.AddNode(1 << c.a_size);
+    auto b_nodes = qr.AddNode(1 << (c.size - c.a_size));
+    Assgn everything = (1 << c.size) - 1;
+    Assgn a_vars = (1 << c.a_size) - 1;
+    Assgn b_vars = everything & ~a_vars;
+    for (Assgn term = 1; term <= everything; ++term) {
+        Assgn term_a = term & a_vars;
+        Assgn term_b = term & b_vars;
+        if (term_a == 0) {
+            // Then term is subset of B, hence is in pairwise cover
+            // Split it into a singleton and rest of term, singleton
+            // is least set bit
+            assert(term_b == term);
+            term_b = (term - 1) & term;
+            if (term_b == 0) { // Then term was singelton
+                int bit_set = __builtin_ctz(term); // Find which one it is
+                assert(term == Assgn(1 << bit_set));
+                qr.AddUnaryTerm(c.vars[bit_set], 0, 2*c.subset_coeffs[term]);
+            } else {
+                R beta_H = c.b_coeffs[term >> c.a_size];
+                auto alpha_H = c.subset_coeffs[term];
+                term_a = term & ~term_b;
+                assert(term_a != 0);
+                assert(term_b < b_vars && term_a < b_vars);
+                assert((term_a | term_b) == term);
+                auto a_node = b_nodes + (term_a >> c.a_size);
+                auto b_node = b_nodes + (term_b >> c.a_size);
+                if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
+                    a_node = c.vars[__builtin_ctz(term_a)];
+                if (__builtin_popcount(term_b) == 1) 
+                    b_node = c.vars[__builtin_ctz(term_b)];
+                qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H + beta_H);
+
+                // Also, add unary and connections to original vars
+                auto var = b_nodes + (term >> c.a_size);
+                qr.AddUnaryTerm(var, 0, beta_H*(2*__builtin_popcount(term) - 1));
+                for (int i = 0; i < c.size; ++i) {
+                    if (term & (1 << i))
+                        qr.AddPairwiseTerm(var, c.vars[i], 0, 0, 0, -2*beta_H);
+                }
+            }
+        } else if (term_b == 0) {
+            // Then term is subset of A, hence is in pairwise cover
+            // Split it into a singleton and rest of term, singleton
+            // is least set bit
+            assert(term_a == term);
+            term_a = (term - 1) & term;
+            if (term_a == 0) { // Then term was singelton
+                int bit_set = __builtin_ctz(term); // Find which one it is
+                assert(term == Assgn(1 << bit_set));
+                qr.AddUnaryTerm(c.vars[bit_set], 0, 2*c.subset_coeffs[term]);
+            } else {
+                auto beta_H = c.a_coeffs[term];
+                auto alpha_H = c.subset_coeffs[term];
+                term_b = term & ~term_a;
+                assert(term_a != 0);
+                assert(term_b < a_vars && term_a < a_vars);
+                assert((term_a | term_b) == term);
+                auto a_node = a_nodes + term_a;
+                auto b_node = a_nodes + term_b;
+                if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
+                    a_node = c.vars[__builtin_ctz(term_a)];
+                if (__builtin_popcount(term_b) == 1) 
+                    b_node = c.vars[__builtin_ctz(term_b)];
+                qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H + beta_H);
+
+                // Also, add unary and connections to original vars
+                auto var = a_nodes + term;
+                qr.AddUnaryTerm(var, 0, beta_H*(2*__builtin_popcount(term) - 1));
+                for (int i = 0; i < c.size; ++i) {
+                    if (term & (1 << i))
+                        qr.AddPairwiseTerm(var, c.vars[i], 0, 0, 0, -2*beta_H);
+                }
+            }
+        } else {
+            auto alpha_H = c.subset_coeffs[term];
+            auto a_node = a_nodes + term_a;
+            if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
+                a_node = c.vars[__builtin_ctz(term_a)];
+            auto b_node = b_nodes + (term_b >> c.a_size);
+            if (__builtin_popcount(term_b) == 1) 
+                b_node = c.vars[__builtin_ctz(term_b)];
+            qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H);
+        }
+    }
+}
+
+template <typename R, typename QR>
+void CliqueToQuadratic(const PairwiseCover<R, 4>& pc, const typename PairwiseCover<R, 4>::Clique& c, QR& qr) {
+    assert(c.size == 4);
+    typedef uint32_t Assgn;
+    auto a_node = qr.AddNode(1);
+    auto b_node = qr.AddNode(1);
+    Assgn a_var = 3;
+    Assgn b_var = 12;
+    R beta_a = c.a_coeffs[3];
+    R beta_b = c.b_coeffs[3];
+    for (int i = 0; i < c.size; ++i)
+        qr.AddUnaryTerm(c.vars[i], 0, 2*c.subset_coeffs[1 << i]);
+
+    // Edges involving a_node
+    qr.AddPairwiseTerm(c.vars[0], c.vars[1], 0, 0, 0, 2*c.subset_coeffs[a_var] + beta_a);
+    qr.AddUnaryTerm(a_node, 0, beta_a*3);
+    qr.AddPairwiseTerm(a_node, 0, 0, 0, 0, -2*beta_a);
+    qr.AddPairwiseTerm(a_node, 1, 0, 0, 0, -2*beta_a);
+
+    // Edges involving b_node
+    qr.AddPairwiseTerm(c.vars[2], c.vars[3], 0, 0, 0, 2*c.subset_coeffs[b_var] + beta_b);
+    qr.AddUnaryTerm(b_node, 0, beta_b*3);
+    qr.AddPairwiseTerm(b_node, 2, 0, 0, 0, -2*beta_b);
+    qr.AddPairwiseTerm(b_node, 3, 0, 0, 0, -2*beta_b);
+    
+    // Rest of the edges
+    qr.AddPairwiseTerm(c.vars[0], c.vars[2], 0, 0, 0, 2*c.subset_coeffs[5]);
+    qr.AddPairwiseTerm(c.vars[0], c.vars[3], 0, 0, 0, 2*c.subset_coeffs[9]);
+    qr.AddPairwiseTerm(c.vars[1], c.vars[2], 0, 0, 0, 2*c.subset_coeffs[6]);
+    qr.AddPairwiseTerm(c.vars[1], c.vars[3], 0, 0, 0, 2*c.subset_coeffs[10]);
+    qr.AddPairwiseTerm(a_node, c.vars[2], 0, 0, 0, 2*c.subset_coeffs[7]);
+    qr.AddPairwiseTerm(a_node, c.vars[3], 0, 0, 0, 2*c.subset_coeffs[11]);
+    qr.AddPairwiseTerm(b_node, c.vars[0], 0, 0, 0, 2*c.subset_coeffs[13]);
+    qr.AddPairwiseTerm(b_node, c.vars[1], 0, 0, 0, 2*c.subset_coeffs[14]);
+    qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*c.subset_coeffs[15]);
 }
 
 template <typename R, int D>
 template <typename QR>
 inline void PairwiseCover<R, D>::ToQuadratic(QR& qr) {
     typedef typename QR::NodeId NodeId;
-    ComputeBeta();
     qr.AddNode(_varCounter);
     // REMEMBER: SCALE ALL COSTS BY 2 WHEN ADDING TO QR
     for (NodeId i = 0; i < _varCounter; ++i)
         qr.AddUnaryTerm(i, 0, 2*_unaryTerms[i]);
     for (const Clique& c: _cliques) {
-        // TODO(afix): note, there's some waste here, since we
-        // don't actually need nodes for the singeltons / empty
-        // subsets. Shouldn't really affect things too much
-        NodeId a_nodes = qr.AddNode(c.a_coeffs.size());
-        NodeId b_nodes = qr.AddNode(c.b_coeffs.size());
-        Assgn everything = (1 << c.size) - 1;
-        Assgn a_vars = (1 << c.a_size) - 1;
-        Assgn b_vars = everything & ~a_vars;
-        for (Assgn term = 1; term <= everything; ++term) {
-            Assgn term_a = term & a_vars;
-            Assgn term_b = term & b_vars;
-            if (term_a == 0) {
-                // Then term is subset of B, hence is in pairwise cover
-                // Split it into a singleton and rest of term, singleton
-                // is least set bit
-                assert(term_b == term);
-                term_b = (term - 1) & term;
-                if (term_b == 0) { // Then term was singelton
-                    int bit_set = __builtin_ctz(term); // Find which one it is
-                    assert(term == Assgn(1 << bit_set));
-                    qr.AddUnaryTerm(c.vars[bit_set], 0, 2*c.subset_coeffs[term]);
-                } else {
-                    R beta_H = c.b_coeffs[term >> c.a_size];
-                    R alpha_H = c.subset_coeffs[term];
-                    term_a = term & ~term_b;
-                    assert(term_a != 0);
-                    assert(term_b < b_vars && term_a < b_vars);
-                    assert((term_a | term_b) == term);
-                    NodeId a_node = b_nodes + (term_a >> c.a_size);
-                    NodeId b_node = b_nodes + (term_b >> c.a_size);
-                    if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
-                        a_node = c.vars[__builtin_ctz(term_a)];
-                    if (__builtin_popcount(term_b) == 1) 
-                        b_node = c.vars[__builtin_ctz(term_b)];
-                    qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H + beta_H);
-
-                    // Also, add unary and connections to original vars
-                    NodeId var = b_nodes + (term >> c.a_size);
-                    qr.AddUnaryTerm(var, 0, beta_H*(2*__builtin_popcount(term) - 1));
-                    for (int i = 0; i < c.size; ++i) {
-                        if (term & (1 << i))
-                            qr.AddPairwiseTerm(var, c.vars[i], 0, 0, 0, -2*beta_H);
-                    }
-                }
-            } else if (term_b == 0) {
-                // Then term is subset of A, hence is in pairwise cover
-                // Split it into a singleton and rest of term, singleton
-                // is least set bit
-                assert(term_a == term);
-                term_a = (term - 1) & term;
-                if (term_a == 0) { // Then term was singelton
-                    int bit_set = __builtin_ctz(term); // Find which one it is
-                    assert(term == Assgn(1 << bit_set));
-                    qr.AddUnaryTerm(c.vars[bit_set], 0, 2*c.subset_coeffs[term]);
-                } else {
-                    R beta_H = c.a_coeffs[term];
-                    R alpha_H = c.subset_coeffs[term];
-                    term_b = term & ~term_a;
-                    assert(term_a != 0);
-                    assert(term_b < a_vars && term_a < a_vars);
-                    assert((term_a | term_b) == term);
-                    NodeId a_node = a_nodes + term_a;
-                    NodeId b_node = a_nodes + term_b;
-                    if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
-                        a_node = c.vars[__builtin_ctz(term_a)];
-                    if (__builtin_popcount(term_b) == 1) 
-                        b_node = c.vars[__builtin_ctz(term_b)];
-                    qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H + beta_H);
-
-                    // Also, add unary and connections to original vars
-                    NodeId var = a_nodes + term;
-                    qr.AddUnaryTerm(var, 0, beta_H*(2*__builtin_popcount(term) - 1));
-                    for (int i = 0; i < c.size; ++i) {
-                        if (term & (1 << i))
-                            qr.AddPairwiseTerm(var, c.vars[i], 0, 0, 0, -2*beta_H);
-                    }
-                }
-            } else {
-                R alpha_H = c.subset_coeffs[term];
-                NodeId a_node = a_nodes + term_a;
-                if (__builtin_popcount(term_a) == 1) // It's a singelton, identify with original var
-                    a_node = c.vars[__builtin_ctz(term_a)];
-                NodeId b_node = b_nodes + (term_b >> c.a_size);
-                if (__builtin_popcount(term_b) == 1) 
-                    b_node = c.vars[__builtin_ctz(term_b)];
-                qr.AddPairwiseTerm(a_node, b_node, 0, 0, 0, 2*alpha_H);
-            }
-        }
+        CliqueToQuadratic(*this, c, qr);
     }
 
     //std::cout << "Constant term: " << _constantTerm << "\n";
