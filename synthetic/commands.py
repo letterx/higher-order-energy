889fc180062668a74adf8cf38a6731b88fb53a6c
Charles Herrmann <irwinherrmann@gmail.com>
2013-10-30 18:35:45 -0400

    updates to graph script

--- synthetic/commands.py
+++ synthetic/commands.py
@@ -35,23 +35,55 @@ for m in allm:
         additional_variables[m][c] = []
         total_edges[m][c] = []
 
+def initfile():
+    filename = 'temp'
+    write(filename)
+    read(filename)
 
-for m in allm:
-    for n in alln:
-        for c in cliques:
-            w, h = c
-            cmd = cmd_template % (m, n, unary, w, h)
-            out = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-            output = out.stdout.read().split('\n')
-            energy[m][c].append(int(output[1].split()[1]))
-            original_variables[m][c].append(int(output[2].split()[2]))
-            labeled[m][c].append(int(output[3].split()[1]))
-            ones[m][c].append(int(output[4].split()[1]))
-            time[m][c].append(float(output[5].split()[1]))
-            additional_variables[m][c].append(int(output[6].split()[2]))
-            total_edges[m][c].append(int(output[7].split()[2]))
-            if n % 100 == 0:
-                print n
+def initrun():
+    for m in allm:
+        for n in alln:
+            for c in cliques:
+                w, h = c
+                cmd = cmd_template % (m, n, unary, w, h)
+                out = subprocess.Popen(cmd.split(), stdout=output)
+                output = out.stdout.read().split('\n')
+                energy[m][c].append(int(output[1].split()[1]))
+                original_variables[m][c].append(int(output[2].split()[2]))
+                labeled[m][c].append(int(output[3].split()[1]))
+                ones[m][c].append(int(output[4].split()[1]))
+                time[m][c].append(float(output[5].split()[1]))
+                additional_variables[m][c].append(int(output[6].split()[2]))
+                total_edges[m][c].append(int(output[7].split()[2]))
+                if n % 100 == 0:
+                    print n
+
+def write(outfile):
+    scan = open(outfile, 'w')
+    for m in allm:
+        for n in alln:
+            for c in cliques:
+                w, h = c
+                cmd = cmd_template % (m, n, unary, w, h)
+                subprocess.Popen(cmd.split(), stdout=scan)
+                if n % 100 == 0:
+                    print n
+
+
+def read(infile):
+    scan = open(infile, 'r')
+    for m in allm:
+        for n in alln:
+            for c in cliques:
+                w, h = c
+                scan.readline()
+                energy[m][c].append(int(scan.readline()[1].split()[1]))
+                original_variables[m][c].append(int(scan.readline()[2].split()[2]))
+                labeled[m][c].append(int(scan.readline()[3].split()[1]))
+                ones[m][c].append(int(scan.readline()[4].split()[1]))
+                time[m][c].append(float(scan.readline()[5].split()[1]))
+                additional_variables[m][c].append(int(scan.readline()[6].split()[2]))
+                total_edges[m][c].append(int(scan.readline()[7].split()[2]))
 
 def clique_size():
     data = additional_variables
@@ -91,7 +123,7 @@ def number_of_nodes():
         plt.plot(alln, data[allm[i]][cliques[0]], colors[i], linestyle='-')
 
     # labels
-    plt.title('Time versus clique size for number of nodes')
+    plt.title('Time versus number of nodes for clique size 2x2')
     plt.ylabel('Time (s)')
     plt.xlabel('Number of nodes')
     plt.legend(allm, loc = 'upper left')
@@ -112,5 +144,5 @@ def number_of_edges():
     plt.legend(allm, loc = 'upper left')
     plt.show()
 
-
-number_of_edges()
+initfile()
+number_of_nodes()
